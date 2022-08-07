8067b9b5c90fd0f85f8417e93bd91cecee53cd9b
user <ersinnml@protonmail.com>
2022-08-07 19:52:30 +0300

    Added arrow icon to Select component

--- src/components/UI/Select/Select.js
+++ src/components/UI/Select/Select.js
@@ -23,6 +23,20 @@ const Select = (props) => {
         }`}
       >
         {props.value ?? ""}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="2 1 20 20"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M17 13l-5 5m0 0l-5-5m5 5V6"
+          />
+        </svg>
       </div>
       {showOptions && (
         <ul className={classes.options}>
